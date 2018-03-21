bc80c3d035773a9e47b77908699752bbc2a59387
windsor80 <windsorjunior@hotmail.com>
2018-03-20 21:31:47 -0400

    updates

--- public/app.js
+++ public/app.js
@@ -1,8 +1,9 @@
 // Grab the articles as a json
+
 $.getJSON("/copa", function(data) {
 
 	console.log(data);
-	
+
   // For each one
   // for (var i = 0; i < data.length; i++) {
   //   // Display the apropos information on the page
